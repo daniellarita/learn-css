c2c07b160c0afb27245c528b13561692e42e6500
Danni Baxter <dbaxter@wework.com>
2018-03-10 19:22:52 -0500

    start resume

--- src/topic_components/ResumeTemplate/ResumeTemplate.js
+++ src/topic_components/ResumeTemplate/ResumeTemplate.js
@@ -13,11 +13,58 @@ const ResumeTemplate = () => {
             <div className="bio">
                 <p>{res.BIO}</p>
             </div>
+            <div className="main-section">
+                <div className="experience">
+                    <h2 className="section-header">Experience</h2>
+                    {res.EXPERIENCE.map(job => {
+                        return (
+                            <div>
+                                <h3>{job.name}, {job.location}</h3>
+                                { job.role.map(role => {
+                                    return (
+                                        <div>
+                                            <h4>{role.name}</h4>
+                                            <h5 className="dates">{role.date}</h5>
+                                            <ul>
+                                                {role.bullets.map(bullet => {
+                                                    return <li>{bullet}</li>
+                                                })}
+                                            </ul>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        );
+                    })}
+                </div>
+                <div className="education">
+                    <h2 className="section-header">Education</h2>
+                    {res.EDUCATION.map(school => {
+                        return (
+                            <div>
+                                <h4>{school.name}, {school.location}</h4>
+                                <h5 className="dates">{school.date}</h5>
+                                <p>{school.description}</p>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
             <div className="skills">
-                <label></label>
-                {res.SKILLS.Proficient.map(skill => {
-                    return <p>{skill}</p>
-                })}
+                <h2 className="section-header">Skills</h2>
+                {Object.keys(res.SKILLS).map(level => {
+                  return ( 
+                    <div>
+                        <label>{level}:</label>
+                        {res.SKILLS[level].map(skill => {
+                            return <p>{skill}</p>
+                        })}
+                        <br></br>
+                    </div>
+                 
+                  );  
+                })
+                }
             </div>
         </div>
     );
--- src/topic_components/ResumeTemplate/resumeContent.js
+++ src/topic_components/ResumeTemplate/resumeContent.js
@@ -1,7 +1,55 @@
 export const NAME="Danni Baxter";
 export const CONTACT_INFO="89 Grattan Street, 1L, Brooklyn, NY 11237, (216) 577-1033, drbaxter821@gmail.com";
-export const BIO="Seeking full stack software engineering role after experiences at WeWork developing www.wework.com, product management and test automation.";
+export const BIO="Seeking full stack software engineering role after experiences at WeWork developing www.wework.com, and working in product management and test automation roles.";
+export const EXPERIENCE=[
+    {
+        name: 'WeWork',
+        location: 'New York, NY',
+        role: [
+            {
+                name: 'Software Engineer',
+                date: 'January 2018 - present',
+                bullets: ['Build events SEO landing page and automate processes around CMS']
+            },
+            {
+                name: 'Software Development Engineer in Test',
+                date: 'October 2015 - January 2018',
+                bullets: ['Build and maintain E2E automation framework and API automation framework across multiple product lines using page object model design pattern, Ruby, Selenium, Capybara, Rspec, Sauce Labs, Jenkins, Java and REST-assured']
+            },
+            {
+                name: 'Associate Product Manager',
+                date: 'June 2015 - October 2015',
+                bullets: ['Improve agile process through use of customizable PM tools (JIRA)', 'Respond to customer support tickets to triage defects and guide product decisions', 'Lead focus groups, usability testing and other user research initiatives']
+            },
+        ],
+    },
+    {
+        name: 'Ernst & Young',
+        location: 'New York, NY',
+        role: [
+            {
+                name: 'Technology Consultant',
+                date: 'September 2014 - June 2015',
+                bullets: ['Analyze data and test IT application controls for financial services client', 'Develop and execute project management procedures for long-term software project'],
+            },
+        ],
+    },
+]
 
+export const EDUCATION=[
+    {
+        name: 'Fullstack Academy',
+        location: 'New York, NY',
+        date: 'August 2016 - March 2017',
+        description: 'This software engineering bootcamp focused on full stack JavaScript web development using Express, React and Node and PostgreSQL. I built multiple projects in small teams including an e-commerce site and a trip finding site that used multiple travel-related APIs to provide a vacation package (with an Airbnb and flight) based on available dates and other preferences.'
+    },
+    {
+        name: 'Lehigh University, College of Business and Economics',
+        location: 'Bethlehem, PA',
+        date: 'August 2010 - May 2014',
+        description: 'Majors: Informations Systems and Finance, Minor: Psychology'
+    },
+]
 export const SKILLS={
     Proficient: ['JavaScript', 'ES6', 'React', 'Redux', 'Node', 'Express', 'Git', 'Ruby', 'Rspec', 'PostgreSQL', 'Sequelize', 'HTML5', 'CSS', 'Bootstrap'],
     Learning: ['Java', 'Rails', 'Mocha', 'Chai', 'Enzyme', 'Storybook'],
