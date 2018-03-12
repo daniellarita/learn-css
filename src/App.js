b2f86d0eb4a2d93988357a40351e239917b4f888
Danni Baxter <dbaxter@wework.com>
2018-03-12 09:55:39 -0400

    ...

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <Router>
-      <div className="App">
+      <div>
         <h3>Topics</h3>
         <ul>
           <li><Link to="/box-display">BoxDisplay</Link></li>
